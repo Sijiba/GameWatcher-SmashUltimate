a65821d63699572e50d80c4bd44085400a7407c8
Spencer Berg <31422889+Sijiba@users.noreply.github.com>
2021-05-27 18:36:41 -0500

    The image comparison goes by a percentage difference threshold now
    Some antialiasing difference or whatever can cause tiny differences from the reference image

--- monitorSmash.py
+++ monitorSmash.py
@@ -4,6 +4,16 @@ import numpy as np
 import sys
 
 
+def is_similar(image1, image2):
+    if image1.shape != image2.shape:
+        return False
+    res = cv2.absdiff(image1, image2)
+    res = res.astype(np.uint8)
+    percentage = (np.count_nonzero(res) * 100) / res.size
+    #print(str(percentage) + "%")
+    return percentage < 5
+
+
 def init_score_files():
     f = open("sharedInfo/gameCount.txt", "w")
     f.write(str(0))
@@ -34,8 +44,8 @@ def increment_file(path):
 def keep_score_in_smash(capture_index=0):
 
     targetImg2 = cv2.imread("images/p1Rank2.png")
-    frame2sizeX = int(targetImg2.shape[1])
-    frame2sizeY = int(targetImg2.shape[0])
+    frameSizeX = int(targetImg2.shape[1])
+    frameSizeY = int(targetImg2.shape[0])
 
     # define a video capture object
     vid = cv2.VideoCapture(capture_index)
@@ -54,27 +64,31 @@ def keep_score_in_smash(capture_index=0):
     while not knowsWinner:
         # Capture the video frame
         ret, frame = vid.read()
-        frame1 = frame[frame1offY:frame1offY + frame2sizeY, frame1offX:frame1offX + frame2sizeX]
-        frame2 = frame[frame1offY:frame1offY + frame2sizeY, frame2offX:frame2offX + frame2sizeX]
-        if not np.bitwise_xor(frame1, targetImg2).any():
+
+        frame1 = frame[frame1offY:frame1offY + frameSizeY, frame1offX:frame1offX + frameSizeX]
+        frame2 = frame[frame1offY:frame1offY + frameSizeY, frame2offX:frame2offX + frameSizeX]
+
+        if is_similar(frame1, targetImg2):
             knowsWinner = True
             winner = 2
-        if not np.bitwise_xor(frame2, targetImg2).any():
+        if is_similar(frame2, targetImg2):
             knowsWinner = True
             winner = 1
 
-    winStr = "P" + str(winner)
-    print(winStr + " wins!")
-    winnerPath = "sharedInfo/" + winStr + ".txt"
-    increment_file(winnerPath)
-    increment_file("sharedInfo/gameCount.txt")
+    if 0 <= winner <= 8:
+        winStr = "P" + str(winner)
+        print(winStr + " wins!")
+        winnerPath = "sharedInfo/" + winStr + ".txt"
+        increment_file(winnerPath)
+        increment_file("sharedInfo/gameCount.txt")
 
-    # wait for screen to blank
-    while not screenBlanked:
-        ret, frame = vid.read()
-        if cv2.countNonZero(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)) == 0:
-            screenBlanked = True
-
+        # wait for screen to blank
+        while not screenBlanked:
+            ret, frame = vid.read()
+            if cv2.countNonZero(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)) == 0:
+                screenBlanked = True
+    else:
+        return False
 
     # After the loop release the cap object
     vid.release()
@@ -83,7 +97,7 @@ def keep_score_in_smash(capture_index=0):
 
 
 if __name__ == "__main__":
-    cam_index = 2
+    cam_index = 0
     print(sys.argv)
     if len(sys.argv) >= 2:
         cam_index = sys.argv[2]
